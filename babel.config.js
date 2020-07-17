3f73ed754009ea493fa2aa1b6d4d9e442ab739d7
liuyi20 <vampire_624@yeah.net>
2020-07-17 09:41:06 +0800

    update babel config, add source type: unambiguous

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   presets: ['@babel/preset-env'],
+  sourceType: 'unambiguous',
   plugins: [
     ['@babel/plugin-syntax-dynamic-import', { useESModules: true }],
     '@babel/plugin-transform-runtime'
